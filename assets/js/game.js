d3d93b80d03844ab60c77f9d07e4aec32ea555f1
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-10-19 13:09:34 -0400

    updated game logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,22 +2,30 @@
 var playerName = window.prompt("What is your robot's name?");
 window.alert(" Enter " + ", " + playerName);
 
+// Player stats
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 20;
+
+// Enemy stats and names
+var enemyNames = ["Meta lord", "Hammera", "War rocket"];
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 // Log multiple values at once
 console.log(playerName, playerAttack, playerHealth);
 
 // Robot enemy health
-var enemyName = "Metal";
-var enemyHealth = 50;
-var enemyAttack = 12;
-var playerMoney = 20;
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
 
-// This creates a function named "fight"
-var fight = function () {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Fighter Robots!");
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+
 
     // Fight or flight
     var promptFight = window.prompt("Would you like to FIGHT or QUIT this battle? Enter 'FIGHT' or 'QUIT' to choose. ");
@@ -27,20 +35,20 @@ var fight = function () {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
         );
 
         // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         } else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         }
 
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
 
         // check player's health
@@ -69,6 +77,21 @@ var fight = function () {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
+
+    //    * Fight all enemy-robots
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames [i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+// This creates a function named "fight"
+for(var i = 0; i < enemyNames.length; i++) {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Fighter Robots!");
+    
+    fight(enemyNames[i]);
+    
 }
 // execute function
 fight();
