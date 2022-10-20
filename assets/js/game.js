70236ff1c5c45c7ed1336fbec0d1f7e58f68d19e
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-10-20 15:26:49 -0400

    multiple rounds added

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,10 +28,10 @@ console.log(enemyNames[3]);
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd like to fight or run
-      var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+      var promptFight = window.prompt('Would you like to FIGHT or QUIT this battle? Enter "FIGHT" or "QUIT" to choose.');
   
       // if player picks "skip" confirm and then stop the loop
-      if (promptFight === "skip" || promptFight === "SKIP") {
+      if (promptFight === "quit" || promptFight === "QUIT") {
         // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
   
@@ -82,9 +82,30 @@ var fight = function(enemyName) {
   };
 // This creates a function named "fight"
 for (var i = 0; i < enemyNames.length; i++) {
+
+    if (playerHealth > 0) {
+    
+    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert(" ROUND " + ( i + 1 + "!" ) );
+    window.alert(" FIGHT! " );
+    
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
     enemyHealth =50;
+
+    // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+}
+    else {
+        window.alert('You have lost your robot in battle! GAME OVER');
+
+        break;
+    }
 }
 // execute function
 fight();
